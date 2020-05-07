0b7e19da79166df54eaa2c4d5ce48c6ecb46468f
ziqianwang9 <33105745+ziqianwang9@users.noreply.github.com>
2020-05-07 11:18:51 +0200

    Update eta_square.m

--- eta_square.m
+++ eta_square.m
@@ -2,7 +2,7 @@ function  f = eta_square( Z,N )
 %calculate a measure of effect size for the Mann-Whitney U statistic. 
 %It's essentially based on eta-squared. 
 %  ?2= Z2/(N-1)
-    f =(Z^2) /  (N-1)
+    f =(Z^2) /  (N-1);
 
 end
 
