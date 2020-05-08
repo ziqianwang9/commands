e64bfda3ce65a1cbf71a3043dc4921550fe7cace
Ziqian Wang <ziqianwang9@gmail.com>
2020-05-08 14:11:41 +0200

    file_A something

---
+++ statistics/theta_square.m
@@ -0,0 +1,8 @@
+function  f = theta_square( Z,N )
+%calculate a measure of effect size for the Mann-Whitney U statistic. 
+%It's essentially based on eta-squared. 
+%  ?2= Z2/(N-1)
+    f =(Z^2) /  (N-1);
+
+end
+
